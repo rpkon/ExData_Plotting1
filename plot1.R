414729b017ae8fb48a8a3411e17186567859ee55
rpkon <rkondapalli@vmware.com>
2017-01-03 23:28:25 +0530

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -3,4 +3,5 @@ sub <- dat[dat$Date %in% c('1/2/2007','2/2/2007'),]
 global_active_power <- as.numeric(sub$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(global_active_power, col ="red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+dev.off()
 
--- plot2.R
+++ plot2.R
@@ -2,6 +2,7 @@ dat <- read.table("household_power_consumption.txt", header=T, stringsAsFactors=
 sub <- dat[dat$Date %in% c('1/2/2007','2/2/2007'),]
 day <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 global_active_power <- as.numeric(sub$Global_active_power)
-png("plot1.png", width=480, height=480)
+png("plot2.png", width=480, height=480)
 plot(day, global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
 
